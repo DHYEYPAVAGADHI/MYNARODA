171669bf4a449790bc0ef6a3acc40523e9699eec
Dhyey Pavagadhi <dhyey@example.com>
2026-08-12 18:44:32 +0530

    Rip out cropper logic and use standard production-safe file uploads

--- config/urls.py
+++ config/urls.py
@@ -120,8 +120,4 @@ if settings.DEBUG:
     except ImportError:
         pass
 
-from django.urls import re_path
-from django.views.static import serve
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
